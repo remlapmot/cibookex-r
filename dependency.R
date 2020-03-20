545e54634d74887fac75b4e1c88ca877ad212030
Tom Palmer <remlapmot@hotmail.com>
2020-03-20 20:27:08 +0000

    Create dependency.R

---
+++ dependency.R
@@ -0,0 +1,8 @@
+if (Sys.info()[['sysname']] == "Windows") options(pkgType = "win.binary")
+
+options(repos = "https://cloud.r-project.org")
+
+if (!requireNamespace("devtools", quietly = TRUE)) install.packages("devtools")
+devtools::install_dev_deps()
+
+if (!tinytex:::is_tinytex()) tinytex::install_tinytex()
