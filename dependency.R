2d5a167f61f29ba740518688c5a00b087dbe4d6a
Tom Palmer <remlapmot@hotmail.com>
2022-02-18 18:13:19 +0000

    Fix end of file and is_tinytex() now exported

--- dependency.R
+++ dependency.R
@@ -5,4 +5,4 @@ options(repos = "https://cloud.r-project.org")
 if (!requireNamespace("devtools", quietly = TRUE)) install.packages("devtools")
 devtools::install_dev_deps()
 
-if (!tinytex:::is_tinytex()) tinytex::install_tinytex()
+if (!tinytex::is_tinytex()) tinytex::install_tinytex()
