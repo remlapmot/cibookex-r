7ded2542c5824848224462b86ad9248538cf3ec4
Tom Palmer <remlapmot@hotmail.com>
2026-04-29 11:52:37 +0100

    Update dependency.R

--- dependency.R
+++ dependency.R
@@ -2,7 +2,8 @@ if (Sys.info()[['sysname']] == "Windows") options(pkgType = "win.binary")
 
 options(repos = "https://cloud.r-project.org")
 
-if (!requireNamespace("devtools", quietly = TRUE)) install.packages("devtools")
-devtools::install_dev_deps()
+if (!requireNamespace("pak", quietly = TRUE)) install.packages("pak")
+#devtools::install_dev_deps()
+pak::local_install_dev_deps()
 
 if (!tinytex::is_tinytex()) tinytex::install_tinytex()
